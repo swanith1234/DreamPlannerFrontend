6233574331f963822071a3c416e04f821e0b02e2
SwanithPidugu <swanith@cricclubs.com>
2026-01-28 12:15:23 +0530

    corrected baseurl

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://dreamplanner-lbm7.onrender.com/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api` || 'https://dreamplanner-lbm7.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
